2d3f1163c6e415a0b21ff479031229e8fd412d65
Robin Ramael <robin.ramael@maykinmedia.nl>
2019-05-03 11:11:23 +0200

    yarr

---
+++ README.md
@@ -0,0 +1,14 @@
+# Git hooks for deadlines with professionals
+
+Copy the pre-commit and pre-commit.d files into your .git/hooks.
+
+The black formatter listens to the $BLACK_LINE_LENGTH variable. Set it to anything else than 100 and suffer the consequences.
+
+
+Feel free to add other hooks here
+
+# TODO
+
+- Use python instead of bash
+- Add other hooks
+- make it easy to add other hooks
---
+++ pre-commit.d/black.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Regexp for grep to only choose some file extensions for formatting
+exts="\.\(py\)$"
+
+# The formatter to use
+formatter="$HOME/.pyenv/shims/black"
+
+options="-l $BLACK_LINE_LENGTH"
+
+# Check availability of the formatter
+if [ -z "$formatter" ]
+then
+  1>&2 echo "$formatter not found. Pre-commit formatting will not be done."
+  exit 1
+fi
+
+
+# Check availability of the formatter
+if [ -z "$formatter" ]
+then
+  1>&2 echo "$formatter not found. Pre-commit formatting will not be done."
+  exit 1
+fi
+
+STAGED_FILES=$(git diff --cached --name-only --diff-filter=ACM "$exts" | sed 's| |\\ |g')
+
+CHANGED_UNSTAGED_FILES=$(git diff --name-only)
+
+
+if [ ! -z "$STAGED_FILES" ]; then
+	if [ ! -z "$CHANGED_UNSTAGED_FILES" ]; then
+		# if there's changed stuff that isn't staged, we dont want to format the unstaged stuff too
+		git stash --keep-index
+	fi
+
+    # Format all selected files
+    echo "$FILES" | xargs "$formatter" "$options"
+
+    # Add back the modified/prettified files to staging
+    echo "$FILES" | xargs git add -u
+
+
+	if [ ! -z "$CHANGED_UNSTAGED_FILES" ]; then
+		# put everything back in its place, hopefully without any merge conflicts...
+		git stash pop
+	fi
+fi
